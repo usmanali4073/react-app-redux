7424fa460ec2584207a8acbe11a46fbf2262b0b2
usman ali <usmanali4073@gmail.com>
2020-05-10 13:36:33 -0400

    test

--- src/components/course/MangeCourse.jsx
+++ src/components/course/MangeCourse.jsx
@@ -1,66 +1,75 @@
 import React, { Component } from "react";
 
-export default class MangeCourse extends Component {
-  constructor(props) {
-    super(props);
+export default import("react-redux").Connect(
+  mapStateToProps,
+  mapDispatchToprops
+)(
+  class MangeCourse extends Component {
+    constructor(props) {
+      super(props);
 
-    this.state = {
-      course: {
-        firstName: "",
-      },
+      this.state = {
+        course: {
+          firstName: "",
+        },
+      };
+    }
+    mapStateToProps(state) {
+      return { course: state.course };
+    }
+    mapDispatchToprops() {}
+
+    handleChnages = (event) => {
+      let course = { ...this.state.course, firstName: event.target.value };
+      this.setState({ course: course });
     };
-  }
+    handleSubmit = (event) => {
+      event.preventDefault();
+      let { firstName } = { ...this.state.course };
+      alert(firstName);
+    };
 
-  handleChnages = (event) => {
-    let course = { ...this.state.course, firstName: event.target.value };
-    this.setState({ course: course });
-  };
-  handleSubmit = (event) => {
-    event.preventDefault();
-    let { firstName } = { ...this.state.course };
-    alert(firstName);
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Courses</h1>
-        <h2>Add A Course</h2>
-        <div className="container">
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-group row">
-              <label
-                htmlFor="inputFirstName"
-                className="col-sm-1-12 col-form-label"
-              >
-                First Name
-              </label>
-              <div className="col-sm-1-12">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="firstName"
-                  id="inputFirstName"
-                  placeholder="First Name"
-                  onChange={this.handleChnages}
-                />
-              </div>
-            </div>
-
-            <div className="form-group row">
-              <div className="offset-sm-2 col-sm-10">
-                <button
-                  type="submit"
-                  className="btn btn-primary"
-                  onSubmit={this.handleSubmit}
+    render() {
+      return (
+        <div>
+          <h1>Courses</h1>
+          <h2>Add A Course</h2>
+          <div className="container">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group row">
+                <label
+                  htmlFor="inputFirstName"
+                  className="col-sm-1-12 col-form-label"
                 >
-                  Submit
-                </button>
+                  First Name
+                </label>
+                <div className="col-sm-1-12">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="firstName"
+                    id="inputFirstName"
+                    placeholder="First Name"
+                    onChange={this.handleChnages}
+                  />
+                </div>
               </div>
-            </div>
-          </form>
+
+              <div className="form-group row">
+                <div className="offset-sm-2 col-sm-10">
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    onSubmit={this.handleSubmit}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
+);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import configureStore from "./redux/configureStore";
+import { Provider as ReduxProivder } from "react-redux";
 
 const store = configureStore();
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ReduxProivder store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ReduxProivder>,
   document.getElementById("root")
 );
